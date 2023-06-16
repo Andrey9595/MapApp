994f61878294c8df57f9e1c2f260bc549331ebdc
Andrey9595 <anb95.95@mail.ru>
2023-06-16 19:19:54 +0300

    added map

--- app/src/main/java/ru/anb/mapapp/ui/MapFragment.kt
+++ app/src/main/java/ru/anb/mapapp/ui/MapFragment.kt
@@ -5,14 +5,62 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.Style
 import ru.anb.mapapp.R
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    var mapView: MapView? = null
+    private lateinit var map: MapboxMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Mapbox.getInstance(requireContext(), getString(R.string.accessToken))
         return inflater.inflate(R.layout.fragment_map, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView = requireActivity().findViewById(R.id.mapView)
+        mapView?.getMapAsync(this)
+    }
+
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        this.map = mapboxMap
+        mapboxMap.setStyle(
+            Style.Builder().fromUri("mapbox://styles/mapbox/streets-v11")
+        ) {
+            // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+            map.locationComponent.activateLocationComponent(LocationComponentActivationOptions.Builder(requireContext(), Style.).build())
+            map.locationComponent.
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
+    }
+
 }
